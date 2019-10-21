5c0d306f91923773c14816cd8343bfa473b027c4
Ronja Faltin <ronjafaltin@gmail.com>
2019-10-21 16:34:28 +0200

    Added save/load  and total holes
    MAde it easier to load and save a model with a specific name. Also made the code warning free. Deleted old code and comments.
    
    Also added a state that is number of holes.

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential, load_model
-from keras.layers import Embedding, Dense
+from keras.layers import Dense
 from collections import deque
 import numpy as np
 import random
@@ -7,7 +7,8 @@ import random
 
 class DQNAgent:
 
-    def __init__(self):
+    def __init__(self, string, load):
+
         # Initialize attributes
         self.start_size = 1000
         self.memory_size = 2000
@@ -23,17 +24,27 @@ class DQNAgent:
         self.epsilon = 0.2
         self.gamma = 0.6
 
-        # Build networks
-        self.q_network = self.build_model()
-        self.target_network = self.build_model()
-        self.alighn_target_model()
+        # If you want to load a saved model
+        if load:
+            # Load the model from string
+            self.q_network = load_model(string)
+            print("Loaded model with name: ", string)
 
-    def store(self, state, action, reward, next_state, terminated, bumpiness, total_height):
-        self.experience_replay.append((state, action, reward, next_state, terminated, bumpiness, total_height))
+            # Build networks
+            self.target_network = self.build_model()
+            self.align_target_model()
+        else:
+            # Build networks
+            self.q_network = self.build_model()
+            self.target_network = self.build_model()
+            self.align_target_model()
+
+    def store(self, state, action, reward, next_state, terminated, bumpiness, total_height, total_holes):
+        self.experience_replay.append((state, action, reward, next_state, terminated, bumpiness, total_height,
+                                       total_holes))
 
     def build_model(self):
         model = Sequential()
-        # Embedding(layer_size, number of dimensions, length of input seq) behover vi?
         model.add(Dense(self.neurons[0], activation='relu'))
         model.add(Dense(self.neurons[0], activation='relu'))
         model.add(Dense(self.neurons[0], activation='linear'))
@@ -42,13 +53,12 @@ class DQNAgent:
 
         return model
 
-    def alighn_target_model(self):
+    def align_target_model(self):
         self.target_network.set_weights(self.q_network.get_weights())
 
     # Exploration function
     def act(self, state):
         """Returns the best state of a given collection of states after exploring"""
-        #state = np.reshape(state, [1, self.state_size])
         if np.random.rand() <= self.epsilon:
             return random.choice(self.actions)
         else:
@@ -57,9 +67,9 @@ class DQNAgent:
 
     def retrain(self, batch_size):
         """Training the agent"""
-        minibatch = random.sample(self.experience_replay, batch_size)
+        mini_batch = random.sample(self.experience_replay, batch_size)
 
-        for state, action, reward, next_state, terminated, bumpiness, total_height in minibatch:
+        for state, action, reward, next_state, terminated, bumpiness, total_height, total_holes in mini_batch:
             target = self.q_network.predict(state)
 
             if terminated:
@@ -70,13 +80,8 @@ class DQNAgent:
 
             self.q_network.fit(state, target, epochs=1, verbose=0)
 
-    def save_model(self):
+    def save_model(self, name):
         # save model and architecture to single file
-        self.q_network.save("big_q_network.h5")
+        self.q_network.save(name)
         print("Saved model to disk")
         # print("Not saved")
-
-    def load_model(self):
-        # load model
-        model = load_model('q_network.h5')
-        print("Loaded model from the disk")
